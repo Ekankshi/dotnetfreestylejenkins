9624e46396ff7d27a673a2dd5eebfa06fea0219f
Ekankshi Sharma <sekankshi12@gmail.com>
2020-07-28 15:04:06 +0530

    Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        int Sum()
+        {
+            int a = 10;
+            int b = 20;
+            int res = a + b;
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("The Sum is: "+ p.Sum());
+            Console.ReadKey();
+        }
+    }
+}
